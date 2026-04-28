3cfc6acc72a8de80e60a5115871aefe35c9d2bde
Vitor Hugo Parnaiba Amorim <hugo.000.vitor3@gmail.com>
2026-04-28 17:03:56 -0300

    alpha 0.1

---
+++ funcoes2.py
@@ -0,0 +1,20 @@
+def exibir_info(**dados):
+    for chave, valor in dados.items():
+        print(f"{chave}: {valor}")
+
+# Criamos um dicionário vazio para armazenar as entradas
+
+info_usuario = {}
+
+print("Digite as informações (ou 'sair' na chave para encerrar):")
+
+while True:
+    chave = input("Nome do campo (ex: Profissão): ")
+    if chave.lower() == 'sair':
+        break
+    valor = input(f"Salário {chave}: ")
+    info_usuario[chave] = valor
+
+# Usamos ** para desempacotar o dicionário como argumentos
+
+exibir_info(**info_usuario)
---
+++ lacos.py
@@ -0,0 +1,24 @@
+# def criar_perfil(nome, idade, cidade):
+#     print(f"{nome}, {idade}, anos, {cidade}")
+
+#     criar_perfil (cidade = "Curitiba", nome "Julia", idade = 25)
+#     #Julia, 25 anos, Curitba -funciona independente da ordem
+
+# def somar_tudo(*numeros):
+#     return sum(numeros)
+# somar_tudo(1,2)
+# somar_tudo(1,2,3,4)
+# somar_tudo(10,20,30)
+# print (somar_tudo)
+
+produto = {"nome": "Teclado", "preco": 150, "em_estoque": True}
+
+# Acessando um valor
+print(produto["nome"])
+
+# Modificando um valor
+produto ["preco"] = 135.00
+print (produto["preco"])
+
+# Adic9onando um novo par
+produto["quantidade"] = 10
